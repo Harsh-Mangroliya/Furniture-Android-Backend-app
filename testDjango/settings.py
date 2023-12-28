316e1bf690e53be7ca5db750c642e7943965fb90
DhrutiVarsani <dhrutivarsani078@gmail.com>
2023-12-28 10:16:03 +0530

    Update settings.py

--- testDjango/settings.py
+++ testDjango/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-a!rsb9)t)wwh-4yoczzkqtrni_5%qg71uhj^a!=z%s@39redxu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','harsh84.azurewebsites.net']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
