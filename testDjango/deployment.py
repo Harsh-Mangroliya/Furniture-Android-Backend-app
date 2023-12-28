d4fd7fcff6346647ed81e37417cc057374baacd9
DhrutiVarsani <dhrutivarsani078@gmail.com>
2023-12-28 10:54:26 +0530

    static files loading attempt 1

---
+++ testDjango/deployment.py
@@ -0,0 +1,22 @@
+import os
+from .settings import *
+from .settings import BASE_DIR
+
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
+
+
+MIDDLEWARE = [
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
--- testDjango/wsgi.py
+++ testDjango/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+setting_module = "testDjango.deployment" if 'WEBSITE_HOSTNAME' in os.environ else 'testDjango.settings'
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testDjango.settings')
 
 application = get_wsgi_application()
