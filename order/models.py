24ed0c399da8d1dc9e9f229a1d19ef3ee3ce07a4
Harsh Mangroliya <harshmangroliya0@gmail.com>
2024-01-01 12:25:17 +0530

    tried new db
    Co-Authored-By: DhrutiVarsani <104624199+DhrutiVarsani@users.noreply.github.com>

--- order/models.py
+++ order/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
     customer = models.ForeignKey('users.user', on_delete=models.CASCADE)
-    order_date = models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    order_date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.customer.username
